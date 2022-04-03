4d24718fbd9694946db95150c36aa0e1a094bd5d
ashkan semsar <ashkansemsar@hotmail.com>
2022-04-03 07:35:16 +0430

    Add project files.

---
+++ Web.Api/Controllers/SecureController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Identity.Identity.PermissionManager;
+using Microsoft.AspNetCore.Authorization;
+using WebFramework.BaseController;
+
+namespace Web.Api.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/Secure")]
+    [Authorize(policy: nameof(ConstantPolicies.DynamicPermission))]
+    public class SecureController : BaseController
+    {
+        [HttpGet("Secure")]
+        public IActionResult Secure()
+        {
+            return Ok();
+        }
+    }
+}
